57835afe8b469e3eb464b1204e2595217dd1c91a
Januuu26 <b77286116@gmail.com>
2026-03-13 23:19:42 +0700

    mengubah kata lulus dan tidak lulus

--- status.cpp
+++ status.cpp
@@ -7,9 +7,9 @@ using namespace std;
     
     string Status_lulus(float r){
         if (r>=60)
-            return "Lulus";
+            return "Selamat Anda Lulus";
         else 
-            return "Tidak Lulus";
+            return "Maaf Anda Tidak Lulus";
         
     }
 
